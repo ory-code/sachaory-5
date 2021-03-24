7d9189f58be318bfb2334f9152107020cc803ac5
Sacha Ory <sachaory@gmail.com>
2021-03-24 18:52:13 +0100

    index integration voir encore font dans figcaption

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
-main ()
-
-async function main() {
-    const articles = await getArticles()
-    console.log(articles);
-    displayArticles(articles)
-}
-
-function getArticles() {
-   return fetch ("http://localhost:3000/api/teddies")
-    .then(function(httpBodyResponse){
-      return httpBodyResponse.json()
+;(async () => {
+    const products = await getProducts()
+    hydratePage(products)
+  })()
+  
+  async function getProducts() {
+    return fetch("http://localhost:3000/api/teddies")
+      .then((httpBodyResponse) => httpBodyResponse.json())
+      .then((products) => products)
+      .catch((error) => {
+        alert(
+          "La connexion au serveur n'a pas pu être effectué. Cela est certainement lié à l'endormissement du serveur Heroku, veuillez attendre quelques secondes le temps qu'il sorte de son lit puis réesayez"
+        )
+      })
+  }
+  
+  function hydratePage(products) {
+    // Remove loading boxes
+    document.getElementById('productsList').innerHTML = ''
+  
+    // Loop over all products and displays them
+    products.forEach((product) => {
+      displayProduct(product)
     })
-    .then(function(articles){
-        console.log(articles);
-    })
-    .catch(function(error){
-        alert(error)
-    })
-} 
-
-function displayArticles() {
-    return ""
-}
+  }
+  
+  function displayProduct(product) {
+    // Get template
+    const templateElt = document.getElementById('product')
+  
+    // Clone template
+    const cloneElt = document.importNode(templateElt.content, true)
+  
+    // Hydrate template
+    cloneElt.getElementById('productImage').src = product.imageUrl
+    cloneElt.getElementById('productName').textContent = product.name
+    cloneElt.getElementById('productPrice').textContent = `${product.price / 100}.00 €`
+    cloneElt.getElementById('productDescription').textContent = product.description
+    cloneElt.getElementById('productLink').href = `/products.html?id=${product._id}`
+  
+    // Display template
+    document.getElementById('productsList').appendChild(cloneElt)
+  }
