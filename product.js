e22d6c6138834c34bb4a59ebb5064646cf212d73
Sacha Ory <sachaory@gmail.com>
2021-04-01 16:01:15 +0200

    correction js + hml product

--- product.js
+++ product.js
@@ -1,33 +1,35 @@
 
-    (async () => {
-        const productId = getProduct()
-        const idData = id
-        hydratePage()
-      })()
-  
-  
-  
-  //Appel URL
-  const id = (new URL(document.location)).searchParams.get('id'); 
-  const getProduct = async () => {
-      const response = await fetch(`http://localhost:3000/api/teddies/${id}`)
-      const product = await response.json()
-      return product
-  }
-  
-  function hydratePage(product) {
-      displayProduct(product)
-  }
-  function displayProduct(product) {
-      // Get template
-      const templateElt = document.getElementById('teddiesProduct')
+const id = (new URL(document.location)).searchParams.get('id'); 
 
-      // Clone template
-      const cloneElt = document.importNode(templateElt.content, true)
+function getProduct() {
+    return fetch(`http://localhost:3000/api/teddies/${id}`)
+        .then((response) => response.json())
+        .then((product) => product)
+        .catch((error) => {
+            alert(
+            "La connexion au serveur n'a pas pu être effectué."
+            )
+        })
+}
 
-      // Hydrate template
-      cloneElt.getElementById('productImage').src = product.imageUrl
-      cloneElt.getElementById('productName').textContent = product.name
-      cloneElt.getElementById('productPrice').textContent = `${product.price / 100}.00 €`
-      cloneElt.getElementById('productDescription').textContent = product.description
-  }
+function hydratePage(prod) {
+    displayProduct(prod)
+}
+
+function displayProduct(product) {
+    const templateElt = document.querySelector('#templateProduct')
+    const cloneElt = document.importNode(templateElt.content, true)
+
+    cloneElt.getElementById('productImage').src = product.imageUrl
+    cloneElt.getElementById('productName').textContent = product.name
+    cloneElt.getElementById('productPrice').textContent = `${product.price / 100}.00 €`
+    cloneElt.getElementById('productDescription').textContent = product.description
+
+    document.getElementById('productsList').appendChild(cloneElt)
+}
+
+
+(async () => {
+    const product = await getProduct()
+    hydratePage(product)
+}) ()
