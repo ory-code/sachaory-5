c32a165fa27b3986050e0066f9ea4349210f0631
Sacha Ory <sachaory@gmail.com>
2021-04-29 10:38:21 +0200

    modif affichage qtt et prix panier

--- frontend/js/basket.js
+++ frontend/js/basket.js
@@ -19,12 +19,11 @@ function onPanier() {
         let td = clone.querySelectorAll("td");
 
         // Prendre le prix total d'un article et additioner le prix total de tous les articles present 
-        prixTotal = result.price * result.quantite / 100 + prixTotal;
-
+        prixTotal = result.price * result.quantity / 100 + prixTotal;
         td[0].textContent = result.name;
-        td[1].textContent = result.quantite;
+        td[1].textContent = result.quantity;
         td[2].textContent = `${result.price / 100} €`;
-        td[3].textContent = `${result.price * result.quantite / 100}`;
+        td[3].textContent = `${result.price * result.quantity / 100}`;
 
         container.appendChild(clone);
     }
@@ -37,13 +36,6 @@ function onPanier() {
 }
 onPanier();
 
-function addEventListeners() {
-    // Purchase button
-    document.getElementById('orderBtn').onclick = (e) => {
-        e.preventDefault()
-        sendOrder()
-    }
-}
 
 // Input validity
 watchValidity(document.getElementById('firstName'), (e) => e.target.value.length > 1)
@@ -54,7 +46,7 @@ watchValidity(document.getElementById('email'), (e) => {
 })
 watchValidity(document.getElementById('address'), (e) => e.target.value.length > 6)
 watchValidity(document.getElementById('zipcode'), (e) => {
-    const zipcodeRegex = /[0-9]{5}(-[0-9]{4})?/
+    const zipcodeRegex = /[0-9]{4}(-[0-9]{4})?/
     return zipcodeRegex.test(e.target.value)
 })
 watchValidity(document.getElementById('city'), (e) => e.target.value.length > 1)
@@ -89,14 +81,16 @@ function neutralInputElt(elt) {
     elt.style.boxShadow = ''
 }
 
-function sendOrder() {
+
+
+const btnOrderElt = document.querySelector('.btnOrder')
+btnOrderElt.addEventListener('submit', () => {
     const firstname = document.getElementById('firstname').value
     const lastname = document.getElementById('lastname').value
     const adress = document.getElementById('adress').value
     const zipcode = document.getElementById('zipcode').value
     const email = document.getElementById('email').value
     const city = document.getElementById('city').value
-
     const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     const zipcodeRegex = /[0-9]{5}(-[0-9]{4})?/
 
@@ -111,21 +105,31 @@ function sendOrder() {
         alert("Veuillez remplir les champs correctements avant de procéder au paiement")
         return
     }
-}
+    const order = {
+        contact: {
+            firstName: firstname,
+            lastName: lastname,
+            address: adress + ' ' + zipcode,
+            city: city,
+            email: email,
+        },
+        products: products,
+    }
+    const requestOptions = {
+        method: 'POST',
+        body: JSON.stringify(order),
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+        },
+    }
+    fetch(`http://localhost:3000/api/teddies/order`, requestOptions)
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json)
+            window.location.href = `order.html`
+        })
+        .catch(() => {
+            alert(error)
+        })
 
-const requestOptions = {
-    method: 'POST',
-    body: JSON.stringify(order),
-    headers: { 'Content-Type': 'application/json; charset=utf-8' },
-  }
-
-  fetch(`${apiUrl}/api/teddies/order`, requestOptions)
-  .then((response) => response.json())
-  .then((json) => {
-    console.log(json)
-    localStorage.removeItem('shoppingCart')
-    window.location.href = `${window.location.origin}/orderStatus.html?orderId=${json.orderId}`
-  })
-  .catch(() => {
-    alert(error)
-  })
+})
