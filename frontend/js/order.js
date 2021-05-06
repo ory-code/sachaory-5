ee211cdcb0a03091e34509f75b9c09c943241bed
Sacha Ory <sachaory@gmail.com>
2021-05-06 21:54:52 +0200

    modif

--- frontend/js/order.js
+++ frontend/js/order.js
@@ -1,39 +1,49 @@
-const getDataOrder = JSON.parse(localStorage.getItem('submit'));
-
-function localVariable() {
-    //Recuperer la valeur de prixTotal dans le localStorage et la Parser
-    //prixTotalJSON = localStorage.getItem("prixTotal");
-    let prixTotal =  JSON.parse(localStorage.getItem("prixTotal"));
-
-    //Selectionné la div corespondante 
-    let prixLocalDiv = document.querySelector(".prixlocal");
- 
-    // Afficher la valeur de prixTotal du localStorage
-    prixLocalDiv.textContent =`${prixTotal} €`;
-} 
-
-localVariable();
-
-
-function orderIdAndReturn() {
-    // Selectionnez la div qui affichera l'OrderId
-    let orderId = document.querySelector(".orderid");
-
-    // Afficher l'OrderId dans la confirmation
-    orderId.textContent = localStorage.getItem("orderId");
-
-    //Supprimer le Panier
-    localStorage.removeItem("article");
-
-
-    // Selection du bouton retour accueil
-    const btnReturn = document.querySelector("#returnaccueil");
-
-    //addEventListener du bouton "COMMANDER"
-    btnReturn.addEventListener("click", (e) => {
-        // Redirection vers la page Confirmation de Commande
-        window.location.href="index.html";
+fetch(`http://localhost:3000/api/teddies/order`)
+    .then(function (res) {
+        if (res.ok) {
+            return res.json();
+        }
     })
-}
+    .then(function (value) {
+        console.log(value);
+    })
+    .catch(function (err) {
+        // Une erreur est survenue
+    });
 
-orderIdAndReturn();
+
+// const getDataOrder = JSON.parse(localStorage.getItem('submit'));
+
+// function localVariable() {
+//     //Recuperer la valeur de prixTotal dans le localStorage et la Parser
+//     //prixTotalJSON = localStorage.getItem("prixTotal");
+//     let prixTotal =  JSON.parse(localStorage.getItem("prixTotal"));
+
+//     //Selectionné la div corespondante 
+//     let prixLocalDiv = document.querySelector(".prixlocal");
+
+//     // Afficher la valeur de prixTotal du localStorage
+//     prixLocalDiv.textContent =`${prixTotal} €`;
+// } 
+
+// localVariable();
+
+
+// function orderIdAndReturn() {
+//     // Selectionnez la div qui affichera l'OrderId
+//     let orderId = document.querySelector(".orderid");
+
+//     // Afficher l'OrderId dans la confirmation
+//     orderId.textContent = localStorage.getItem("orderId");
+
+//     // Selection du bouton retour accueil
+//     const btnReturn = document.querySelector("#returnaccueil");
+
+//     //addEventListener du bouton "COMMANDER"
+//     btnReturn.addEventListener("click", (e) => {
+//         // Redirection vers la page Confirmation de Commande
+//         window.location.href="index.html";
+//     })
+// }
+
+// orderIdAndReturn();
