a86113ff52707997b78e90e939f583a341110861
Sacha Ory <sachaory@gmail.com>
2021-05-12 22:02:15 +0200

    validation envoye data formulaire

--- frontend/js/basket.js
+++ frontend/js/basket.js
@@ -29,9 +29,6 @@ function onPanier() {
         }
         container.appendChild(clone);
     }
-    
-
-
     let prixTotalDiv = document.querySelector(".prixtotal");
     prixTotalDiv.textContent = `${prixTotal} €`;
 
@@ -40,50 +37,46 @@ function onPanier() {
 }
 onPanier();
 
-
 // // Input validity
-// watchValidity(document.getElementById('firstName'), (e) => e.target.value.length > 1)
-// watchValidity(document.getElementById('lastName'), (e) => e.target.value.length > 1)
-// watchValidity(document.getElementById('email'), (e) => {
-//     const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-//     return emailRegex.test(e.target.value)
-// })
-// watchValidity(document.getElementById('address'), (e) => e.target.value.length > 6)
-// watchValidity(document.getElementById('zipcode'), (e) => {
-//     const zipcodeRegex = /[0-9]{4}(-[0-9]{4})?/
-//     return zipcodeRegex.test(e.target.value)
-// })
-// watchValidity(document.getElementById('city'), (e) => e.target.value.length > 1)
+function watchValidity(elt, condition) {
+    watchValidity(document.getElementById('firstName'), (e) => e.target.value.length > 1)
+    watchValidity(document.getElementById('lastName'), (e) => e.target.value.length > 1)
+    watchValidity(document.getElementById('email'), (e) => {
+        const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+        return emailRegex.test(e.target.value)
+    })
+    watchValidity(document.getElementById('address'), (e) => e.target.value.length > 6)
+    watchValidity(document.getElementById('city'), (e) => e.target.value.length > 1)
 
-// function watchValidity(elt, condition) {
-//     elt.oninput = (e) => {
-//         if (condition(e)) {
-//             validInputElt(e.target)
-//         } else {
-//             neutralInputElt(e.target)
-//         }
-//     }
-//     elt.onblur = (e) => {
-//         if (!condition(e)) {
-//             invalidInputElt(e.target)
-//         }
-//     }
-// }
 
-// function validInputElt(elt) {
-//     elt.style.border = 'solid 1px green'
-//     elt.style.boxShadow = '#00800066 0px 0px 4px'
-// }
+    elt.oninput = (e) => {
+        if (condition(e)) {
+            validInputElt(e.target)
+        } else {
+            neutralInputElt(e.target)
+        }
+    }
+    elt.onblur = (e) => {
+        if (!condition(e)) {
+            invalidInputElt(e.target)
+        }
+    }
+}
 
-// function invalidInputElt(elt) {
-//     elt.style.border = 'solid 1px red'
-//     elt.style.boxShadow = 'rgba(128, 0, 0, 0.4) 0px 0px 4px'
-// }
+function validInputElt(elt) {
+    elt.style.border = 'solid 1px green'
+    elt.style.boxShadow = '#00800066 0px 0px 4px'
+}
 
-// function neutralInputElt(elt) {
-//     elt.style.border = ''
-//     elt.style.boxShadow = ''
-// }
+function invalidInputElt(elt) {
+    elt.style.border = 'solid 1px red'
+    elt.style.boxShadow = 'rgba(128, 0, 0, 0.4) 0px 0px 4px'
+}
+
+function neutralInputElt(elt) {
+    elt.style.border = ''
+    elt.style.boxShadow = ''
+}
 
 
 
@@ -97,17 +90,16 @@ btnOrderElt.addEventListener('click', (event) => {
     const city = document.getElementById('city').value
     const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
 
-    // if ((
-    //         firstname.length > 1 &&
-    //         lastname.length > 1 &&
-    //         emailRegex.test(email) &&
-    //         address.length > 6 &&
-    //         zipcodeRegex.test(zipcode) &&
-    //         city.length > 1
-    //     )) {
-    //     alert("Veuillez remplir les champs correctements avant de procéder au paiement")
-    //     return
-    // }
+    if ((
+            firstname.length > 1 &&
+            lastname.length > 1 &&
+            emailRegex.test(email) &&
+            address.length > 6 &&
+            city.length > 1
+        )) {
+        alert("Veuillez remplir les champs correctements avant de procéder au paiement")
+        return
+    }
 
 
     //faire une boucle for localstorage id avec push 
@@ -133,7 +125,7 @@ btnOrderElt.addEventListener('click', (event) => {
         .then((response) => response.json())
         .then((json) => {
             console.log(json)
-            //window.location.href = `order.html`
+            window.location.href = `order.html`
         })
         .catch(() => {
             alert(error)
